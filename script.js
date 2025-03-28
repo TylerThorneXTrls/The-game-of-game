8d284004f7a527357ddcbd18c8b2b083c1ec3d84
TylerThorneXTrls <thornes101@gmail.com>
2025-03-28 16:55:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,273 @@
+
+let lootpool2 = [
+    { "rarity": "common", "randomItem": "Pump Shotgun", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Assault Rifle", "value": 0.7 },
+    { "rarity": "common", "randomItem": "Pistol", "value": 0.6 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.9 },
+    { "rarity": "common", "randomItem": "Tactical Shotgun", "value": 0.8 },
+    { "rarity": "common", "randomItem": "Burst Assault Rifle", "value": 0.6 },
+    { "rarity": "common", "randomItem": "Revolver", "value": 0.3 },
+    { "rarity": "common", "randomItem": "Hunting Rifle", "value": 0.4 },
+    { "rarity": "common", "randomItem": "Submachine Gun", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Heavy Sniper", "value": 0.1 },
+    { "rarity": "common", "randomItem": "Thermal AR", "value": 0.2 },
+    { "rarity": "common", "randomItem": "Pump Shotgun", "value": 0.4 },
+    { "rarity": "common", "randomItem": "Assault Rifle", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Tactical Shotgun", "value": 0.7 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.3 },
+    { "rarity": "common", "randomItem": "Burst Assault Rifle", "value": 0.6 },
+    { "rarity": "common", "randomItem": "Pistol", "value": 0.8 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.1 },
+    { "rarity": "common", "randomItem": "Pump Shotgun", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Assault Rifle", "value": 0.8 },
+    { "rarity": "common", "randomItem": "Pistol", "value": 0.9 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.9 },
+    { "rarity": "common", "randomItem": "Tactical Shotgun", "value": 0.3 },
+    { "rarity": "common", "randomItem": "Burst Assault Rifle", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Revolver", "value": 0.6 },
+    { "rarity": "common", "randomItem": "Hunting Rifle", "value": 0.7 },
+    { "rarity": "common", "randomItem": "Submachine Gun", "value": 0.4 },
+    { "rarity": "common", "randomItem": "Heavy Sniper", "value": 0.2 },
+    { "rarity": "common", "randomItem": "Thermal AR", "value": 0.8 },
+    { "rarity": "common", "randomItem": "Pump Shotgun", "value": 0.2 },
+    { "rarity": "common", "randomItem": "Assault Rifle", "value": 0.9 },
+    { "rarity": "common", "randomItem": "Tactical Shotgun", "value": 0.2 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.4 },
+    { "rarity": "common", "randomItem": "Burst Assault Rifle", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Pistol", "value": 0.5 },
+    { "rarity": "common", "randomItem": "SMG", "value": 0.5 },
+	{ "rarity": "common", "randomItem": "Crossbow", "value": 0.5 },
+    { "rarity": "common", "randomItem": "Bow", "value": 0.7 },
+    { "rarity": "common", "randomItem": "Grenade", "value": 0.6 },
+
+    { "rarity": "uncommon", "randomItem": "Tactical Shotgun", "value": 1.1 },
+    { "rarity": "uncommon", "randomItem": "Submachine Gun", "value": 1.2 },
+    { "rarity": "uncommon", "randomItem": "Revolver", "value": 1.3 },
+    { "rarity": "uncommon", "randomItem": "Pump Shotgun", "value": 1.4 },
+    { "rarity": "uncommon", "randomItem": "Hunting Rifle", "value": 1.5 },
+    { "rarity": "uncommon", "randomItem": "Assault Rifle", "value": 1.6 },
+    { "rarity": "uncommon", "randomItem": "SMG", "value": 1.7 },
+    { "rarity": "uncommon", "randomItem": "Burst Assault Rifle", "value": 1.8 },
+    { "rarity": "uncommon", "randomItem": "Submachine Gun", "value": 1.9 },
+    { "rarity": "uncommon", "randomItem": "Heavy Sniper", "value": 1.2 },
+    { "rarity": "uncommon", "randomItem": "Thermal AR", "value": 1.4 },
+    { "rarity": "uncommon", "randomItem": "Revolver", "value": 1.6 },
+    { "rarity": "uncommon", "randomItem": "Pump Shotgun", "value": 1.1 },
+    { "rarity": "uncommon", "randomItem": "Hunting Rifle", "value": 1.8 },
+    { "rarity": "uncommon", "randomItem": "Assault Rifle", "value": 1.9 },
+    { "rarity": "uncommon", "randomItem": "Burst Assault Rifle", "value": 1.5 },
+    { "rarity": "uncommon", "randomItem": "SMG", "value": 1.1 },
+
+    { "rarity": "rare", "randomItem": "Heavy Sniper", "value": 2.1 },
+    { "rarity": "rare", "randomItem": "Scar", "value": 2.2 },
+    { "rarity": "rare", "randomItem": "Thermal AR", "value": 2.3 },
+    { "rarity": "rare", "randomItem": "Hunting Rifle", "value": 2.4 },
+    { "rarity": "rare", "randomItem": "Suppressed AR", "value": 2.5 },
+    { "rarity": "rare", "randomItem": "Scoped AR", "value": 2.6 },
+    { "rarity": "rare", "randomItem": "Infantry Rifle", "value": 2.7 },
+    { "rarity": "rare", "randomItem": "Bolt-Action Sniper", "value": 2.8 },
+    { "rarity": "rare", "randomItem": "Lmg", "value": 2.9 },
+    { "rarity": "rare", "randomItem": "Storm Scout Sniper Rifle", "value": 2.3 },
+    { "rarity": "rare", "randomItem": "C4", "value": 2.5 },
+    { "rarity": "rare", "randomItem": "Impulse Grenade", "value": 2.7 },
+
+    { "rarity": "epic", "randomItem": "Rocket Launcher", "value": 3.1 },
+    { "rarity": "epic", "randomItem": "Suppressed Sniper Rifle", "value": 3.2 },
+    { "rarity": "epic", "randomItem": "FAMAS", "value": 3.3 },
+    { "rarity": "epic", "randomItem": "Pump Shotgun", "value": 3.4 },
+    { "rarity": "epic", "randomItem": "Heavy Shotgun", "value": 3.5 },
+    { "rarity": "epic", "randomItem": "Bolt-Action Sniper", "value": 3.6 },
+    { "rarity": "epic", "randomItem": "Storm Scout Sniper Rifle", "value": 3.7 },
+    { "rarity": "epic", "randomItem": "Exotic Storm Scout Sniper Rifle", "value": 3.8 },
+    { "rarity": "epic", "randomItem": "Minigun", "value": 3.9 },
+    { "rarity": "epic", "randomItem": "Suppressed Pistol", "value": 4.0 },
+    { "rarity": "epic", "randomItem": "Chug Jug", "value": 4.1 },
+    { "rarity": "epic", "randomItem": "Exotic Chug Jug", "value": 4.2 },
+
+    { "rarity": "legendary", "randomItem": "Gold Scar", "value": 5.0 },
+    { "rarity": "legendary", "randomItem": "Drum Gun", "value": 5.1 },
+    { "rarity": "legendary", "randomItem": "Infinity Blade", "value": 5.2 },
+	
+
+    { "rarity": "uncommon", "randomItem": "Suppressed SMG", "value": 1.4 },
+    { "rarity": "uncommon", "randomItem": "Tactical Submachine Gun", "value": 1.5 },
+
+    { "rarity": "rare", "randomItem": "P90", "value": 2.0 },
+    { "rarity": "rare", "randomItem": "Dragon's Breath Shotgun", "value": 2.5 },
+
+    { "rarity": "epic", "randomItem": "Guided Missile Launcher", "value": 3.4 },
+    { "rarity": "epic", "randomItem": "The Dub", "value": 3.9 },
+
+    { "rarity": "legendary", "randomItem": "Pump Shotgun Gold", "value": 5.3 },
+    { "rarity": "legendary", "randomItem": "Quad Launcher", "value": 5.0 },
+];
+
+
+
+let loot = [];
+let html = '';
+let money = 500
+let multiplier = 1 
+let bonus = 0
+let multiplierCost = 150
+let startTime = new Date()
+let addMoneyCost = 200
+let noCommon = false
+let auto = false
+
+
+
+
+
+function Press() {
+	let randomItem = randomizer();
+	loot.push(randomItem)
+	display();
+}
+function randomizer() {
+	let item = lootpool2[Math.floor(Math.random()*lootpool2.length)]
+	return item
+}
+function display() {
+	html = '';
+
+	loot.forEach((item,index) => {
+		console.log(item)
+		
+		html += `<div class="${item.rarity}"><div class="${item.rarity}"><h3>${item.rarity}</h3><h3>${item.randomItem}</h3><h4>Value ${item.value}</h4></div><button onclick="sell(${index},'${item.value}')">Sell</button></div>`;
+	});
+	document.getElementById('item').innerHTML = html
+	document.getElementById('money').innerHTML = money.toFixed(0)+'$';
+	document.getElementById('multiplierCost').innerHTML = multiplierCost.toFixed(2)+'$'
+	document.getElementById('multiplier').innerHTML = `Multiplier ${multiplier}`
+	document.getElementById('addMoneyCost').innerHTML = addMoneyCost.toFixed(2)+"$"
+    document.getElementById('goal').textContent = `${Math.round(money)}/100000`
+    
+    
+
+}
+function sell(index,value){
+	console.log(value)
+	
+	loot.splice(index,1)
+	money += ((1+bonus)*value)*multiplier
+	console.log(money)
+	display()
+	if(money >= 100000){
+		let time = parseFloat(startTime - new Date())
+		console.log(time)
+		win(time)
+	}
+}
+
+function sellAll(){
+	while(loot.length>0){
+	for(i=0;i<loot.length;i++){
+		let vlaue =loot[i].value
+		sell(i,vlaue)
+	}}
+	
+}
+function common(){
+
+	if(money>=200&&!noCommon){
+		money -= 200
+		document.getElementById('common').style.backgroundColor ="red"
+	
+	lootpool2.forEach((item,index)=>{
+		
+		while(lootpool2[0].rarity=== "common"){
+
+		
+		if(item.rarity==="common"){
+			lootpool2.splice(index,1)
+
+
+		}
+		
+	}}
+	
+	)}
+	console.log(lootpool2)
+	noCommon = true
+	display()
+}
+
+		
+	
+	display()
+	
+
+
+function addMultipler(){
+	if(money>=multiplierCost){
+		multiplierCost *= 1.2
+		money -= multiplierCost
+
+		multiplier += 1
+		console.log(multiplier)
+		display()
+	}
+}
+function win(time){
+	finishtime = Math.abs(time / 1000)
+	console.log(finishtime)
+	document.getElementById('left').innerHTML = `<h1 id="winscreen">your time is ${finishtime} seconds</h1>`
+}
+function addMoney(){
+	if(money >= addMoneyCost){
+
+	money -= addMoneyCost;
+	
+	addMoneyCost *= 1.3;
+	bonus += 3;
+	display()
+	};
+
+
+};
+
+
+addEventListener("keyup",(button)=>{
+	console.log(button)
+	if(button.code === "Space"){
+		Press()
+	}
+
+	
+});	
+display()
+
+
+function Auto(){
+	if((money >= 500)&&(!auto)){
+	console.log(money)
+	
+	money -= 500
+	window.setInterval(Press,500)
+	auto = true
+	document.getElementById('auto').style.backgroundColor="red"
+	display()
+
+}
+}
+addEventListener("keyup",(button)=>{
+	console.log(button)
+	if(button.key === "Backspace"){
+		sellAll()
+	}
+
+	
+});	
+
+
+
+
+
+
+
+
+
+
+
+
